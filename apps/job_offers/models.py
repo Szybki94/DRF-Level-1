215a5268f3f4291067ef57e9d136d91a53cfa90d
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2024-01-13 21:53:43 +0100

    feat: JobOffer model fields draft

--- apps/job_offers/models.py
+++ apps/job_offers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class JobOffer(models.Model):
+    company_name = None
+    company_email = None
+    job_title = None
+    job_description = None
+    salary = None
+    city = None
+    state = None
+    created_at = None
+    available = None
+
+# Modele:
+# Dla projektu wymagany jest tylko jeden model, który można nazwać JobOffer. Powinien on zawierać następujące pola:
+# - company_name
+# - company_email
+# - job_title
+# - job_description
+# - salary
+# - city
+# - state
+# - created_at
+# - available
