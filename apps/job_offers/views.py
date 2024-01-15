61c37cba934bcd91a7f4f1faf399679ef4b4cea3
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2024-01-15 22:07:55 +0100

    feat: job list created and post method for creating offer

--- apps/job_offers/views.py
+++ apps/job_offers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.job_offers.models import JobOffer
+from apps.job_offers.serializers import JobOfferSerializers
+
+
+class JobOfferListApiView(APIView):
+
+    def get(self, request):
+        query = JobOffer.objects.all()
+        serializer = JobOfferSerializers(query, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = JobOfferSerializers(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
